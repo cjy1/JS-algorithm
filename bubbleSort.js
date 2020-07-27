109d738c8e23b0c6716d29276b17d9db189f293e
地念 <dinian@helijia.com>
2020-07-27 11:02:37 +0800

    冒泡排序

---
+++ bubbleSort.js
@@ -0,0 +1,15 @@
+const bubbleSort = arr => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len-1-i; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+export default bubbleSort;
